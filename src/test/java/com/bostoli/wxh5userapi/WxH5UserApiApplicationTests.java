63aba09454320b01cec1e8e8bbc95058c532f55c
Bosto <287489004@qq.com>
2020-07-17 18:00:10 +0800

    add test case

---
+++ src/test/java/com/bostoli/wxh5userapi/WxH5UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bostoli.wxh5userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxH5UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
